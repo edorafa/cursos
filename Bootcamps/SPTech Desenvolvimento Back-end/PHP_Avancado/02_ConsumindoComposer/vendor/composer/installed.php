e4167575a6bf587bb2ce80b90cab4ff1358f89f4
Rafael Sevilha <54326621+edorafa@users.noreply.github.com>
2021-02-03 14:17:01 -0300

    consumindo composer

---
+++ Bootcamps/SPTech Desenvolvimento Back-end/PHP_Avancado/02_ConsumindoComposer/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1a97d084606e16c66303dbcd34de14789d44a0cc',
+    'name' => 'edora/02_consumindo-composer',
+  ),
+  'versions' => 
+  array (
+    'edora/02_consumindo-composer' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1a97d084606e16c66303dbcd34de14789d44a0cc',
+    ),
+    'wead/digital-cep' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '63b5624635c0c1e0f2020f672dcfd235dd9834ff',
+    ),
+  ),
+);
---
+++ Bootcamps/SPTech Desenvolvimento Back-end/PHP_Avancado/02_ConsumindoComposer/vendor/wead/digital-cep/README.md
@@ -0,0 +1,8 @@
+# Consumindo Composer
+
+Comandos necessários:
+
+- composer init
+- composer require wead/digital-cep
+- use Wead\DigitalCep\Search em index.php
+- chamar a classe e função
